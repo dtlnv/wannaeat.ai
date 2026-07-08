d6e3c5a1971a8d76b95e89ad862e099f0cf27392
Dima T. <detelnov@gmail.com>
2026-07-08 17:09:14 +0200

    Update server configuration: set default PORT to 3010, remove asyncHandler utility, and simplify API routing

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,14 @@
 import express from "express";
 import recipeRouter from "./routes/recipe";
-import { asyncHandler } from "./utils/async.handler";
 import { errorMiddleware } from "./utils/error.middleware";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api", asyncHandler(recipeRouter));
+app.use("/api", recipeRouter);
 app.use(errorMiddleware);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3010;
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
